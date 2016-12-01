9fb15a6e9d628a59da533af0511f48732b03a0a5
Ben Keith <ben@benlk.com>
2016-12-01 00:40:21 -0500

    INN_MEMBER and INN_HOSTED

---
+++ wp-content/themes/montana/functions.php
@@ -0,0 +1,10 @@
+<?php
+// This site is an INN Member
+if ( !defined( 'INN_MEMBER' ) ) {
+    define( 'INN_MEMBER', true) ;
+}
+
+// This site is hosted by INN
+if ( !defined( 'INN_HOSTED' ) ) {
+    define( 'INN_HOSTED', true) ;
+}
